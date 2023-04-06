ce4110fef3eaf9e8b4f157963d709b6d1c9cabba
Davi <davifigueiredo0195@gmail.com>
2023-04-05 21:56:44 -0300

    operador while

---
+++ Frontend/05-04-2023/while.js
@@ -0,0 +1,16 @@
+// Exemplos utilizando While e Do While
+
+let salario = 5001;
+
+while (salario < 5000) {
+    salario += 200;
+    console.log(salario)
+}
+
+//Exemplo com o do while
+let aumento  = 301;
+
+do {
+    aumento += 50;
+    console.log(aumento);
+}    while (aumento < 300);
