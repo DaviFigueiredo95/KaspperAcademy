42d033ec2a37842e818f8ff2c502a4f5c7af86ed
Davi <davifigueiredo0195@gmail.com>
2023-04-12 21:33:37 -0300

    datas em js

---
+++ Frontend/12-04-2023/datas.js
@@ -0,0 +1,9 @@
+// Trabalhando com Datas em Javascript
+const data = new Date();
+console.log(data);
+// Recuperando informações da data
+console.log(data.getDate());
+console.log(data.getDay());
+console.log(data.getMonth() + 1);
+console.log(data.getFullYear());
+console.log(data.getHours());
