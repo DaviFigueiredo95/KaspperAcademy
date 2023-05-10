1d4ca52e98f7a8c218b2555dc95341e31709fec6
Davi <davifigueiredo0195@gmail.com>
2023-05-10 20:47:39 -0300

    criando tabela em MySQL

---
+++ BancoDeDados/10-05-2023.sql
@@ -0,0 +1,13 @@
+# create database aula_10_05_2023db;
+
+# use aula_10_05_2023db;
+create table clientes (
+rg int,
+nome varchar(60),
+telefone varchar(15),
+endereco varchar(80),
+primary key (rg)
+);
+
+
+
