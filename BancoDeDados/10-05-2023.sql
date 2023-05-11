13e1601c234bfad9ccd9de28b43cb6ea71f9d08d
Davi <davifigueiredo0195@gmail.com>
2023-05-10 21:06:42 -0300

    tabelas MySQL

--- BancoDeDados/10-05-2023.sql
+++ BancoDeDados/10-05-2023.sql
@@ -1,13 +1,32 @@
 # create database aula_10_05_2023db;
 
 # use aula_10_05_2023db;
-create table clientes (
+
+# Criação de tabela cliente
+/* create table clientes (
 rg int,
 nome varchar(60),
 telefone varchar(15),
 endereco varchar(80),
 primary key (rg)
-);
+); */
 
+/* create table produtos (
+sku int,
+nome varchar(60),
+tipo varchar(15),
+preco decimal(6, 2),
+quantidade int,
+primary key (sku)
+); */
 
+# Criando a tabela vendas e itens_venda
+ create table venda (
+ id int,
+ data_compra date,
+ valor_total decimal(6, 2),
+ rg int,
+ primary key (id),
+ foreign key (rg) references
+ );
 
