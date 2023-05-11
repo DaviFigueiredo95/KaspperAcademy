2b948651d9a4d98997efba4c651999688e28e665
Davi <davifigueiredo0195@gmail.com>
2023-05-10 21:47:38 -0300

    exclusão no MySQL

--- BancoDeDados/10-05-2023.sql
+++ BancoDeDados/10-05-2023.sql
@@ -30,7 +30,7 @@ primary key (sku)
  foreign key (rg) references clientes (rg)
  ); */
  
- create table itens_venda (
+ /* create table itens_venda (
  id_item int,
  sku int,
  id_venda int,
@@ -39,6 +39,22 @@ primary key (sku)
  primary key (id_item),
  foreign key (sku) references produtos (sku),
  foreign key (id_venda) references venda (id)
- );
+ ); */
 
-insert into
+# insert into produtos (sku, nome, tipo) values(3, 'Vaso mineiro', 'vaso')
+
+# select * from produtos;
+# select nome, tipo from produtos;
+
+# Atualizar um registro da Tabela
+/* update produtos
+set quantidade = 10
+where sku = 1; */
+
+# select * from produtos;
+
+# Exclusão de Registro
+# delete from produtos where sku = 2;
+
+# Usando o operador like
+select nome from produtos where nome
