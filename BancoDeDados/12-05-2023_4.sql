aacd74db7f805d3dc4c819fca5f93823533a9efc
Davi <davifigueiredo0195@gmail.com>
2023-05-12 20:42:39 -0300

    SELECT JOIN

--- BancoDeDados/12-05-2023_4.sql
+++ BancoDeDados/12-05-2023_4.sql
@@ -18,3 +18,12 @@ where cidade.uf = estado.id and estado.id = 18;
 select cidade.nome, estado.nome from cidade
 inner join estado on cidade.uf = estado.id
 where estado.id = 18;
+
+# Contagem de registros
+SELECT COUNT(CIDADE.NOME) FROM CIDADE
+INNER JOIN ESTADO ON CIDADE.UF = ESTADO.ID WHERE ESTADO.ID = 18;
+
+# Agrupando
+SELECT COUNT(CIDADE.NOME) AS TOTAL_CIDADES, ESTADO.NOME FROM CIDADE
+INNER JOIN ESTADO ON CIDADE.UF = ESTADO.ID WHERE ESTADO.ID = 11
+GROUP BY ESTADO.NOME;
