4cac115afacc8b7604ef8f7aa363aeb6eb92ef95
Davi <davifigueiredo0195@gmail.com>
2023-05-17 20:36:41 -0300

    string em java

---
+++ Backend/fundamentos/fundamentos/src/Stringue.java
@@ -0,0 +1,15 @@
+public class Stringue {
+    public static void main(String[] args) {
+        String palavra = "Kaspper Academy";
+        System.out.println(palavra);
+
+        System.out.println(palavra.toUpperCase());
+        System.out.println(palavra.toLowerCase());
+        System.out.println("Quantidade de letras: " + palavra.length());
+
+        String x = "20";
+        int y = 3;
+        String z = x + y;
+        System.out.println("Qual o valor de z: " + z);
+    }
+}
