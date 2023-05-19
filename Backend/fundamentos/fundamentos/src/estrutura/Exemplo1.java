8f928edcc45a465c6405cf7c855239d5ad012f6b
Davi <davifigueiredo0195@gmail.com>
2023-05-19 20:33:03 -0300

    java

--- Backend/fundamentos/fundamentos/src/estrutura/Exemplo1.java
+++ Backend/fundamentos/fundamentos/src/estrutura/Exemplo1.java
@@ -1,5 +1,7 @@
 package estrutura;
 
+import java.util.zip.Deflater;
+
 public class Exemplo1 {
     public static void main(String[] args) {
         int contador = 0;
@@ -15,5 +17,29 @@ public class Exemplo1 {
             System.out.println("Contador2: " + contador2);
             contador2++;
         } while (contador2 < 5);
+
+        // Laço utilizando o for
+        for(int i = 0; i < 5; i++) {
+            System.out.println("i: " + i);
+        }
+
+        //System.out.println(i);
+
+        // Switch
+        int opcao = 3;
+
+        switch(opcao) {
+            case 1:
+                System.out.println("Opção 1 selecionada");
+                break;
+            case 2:
+                System.out.println("Opção 2 selecionada");
+                break;
+            case 3:
+                System.out.println("Opção 3 selecionada");
+                break;
+            default:
+                System.out.println("Outra opção selecionada!");
+        }
     }
 }
