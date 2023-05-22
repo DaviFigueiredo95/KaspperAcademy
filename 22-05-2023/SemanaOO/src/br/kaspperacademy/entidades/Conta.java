76b6426e7eb05636aee66555abd6a0729755b95a
Davi <davifigueiredo0195@gmail.com>
2023-05-22 19:44:30 -0300

    teste conta

---
+++ 22-05-2023/SemanaOO/src/br/kaspperacademy/entidades/Conta.java
@@ -0,0 +1,7 @@
+package br.kaspperacademy.entidades;
+
+public class Conta {
+    public String titular;
+    public String numero;
+    public Double saldo;
+}
---
+++ 22-05-2023/SemanaOO/src/br/kaspperacademy/principal/TesteConta.java
@@ -0,0 +1,13 @@
+package br.kaspperacademy.principal;
+
+import br.kaspperacademy.entidades.Conta;
+
+public class TesteConta {
+    public static void main(String[] args) {
+        // Instanciando um objeto
+        Conta c1 = new Conta();
+        c1.titular = "Bill Gates";
+        c1.numero = "95972000XP";
+        c1.saldo = 100000.00;
+    }
+}
