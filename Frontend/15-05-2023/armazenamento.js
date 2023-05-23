9fd45aa752816fa020a69ea01ecca2af7ec9e9b8
Davi <davifigueiredo0195@gmail.com>
2023-05-22 22:29:22 -0300

    localStorage

--- Frontend/15-05-2023/armazenamento.js
+++ Frontend/15-05-2023/armazenamento.js
@@ -7,3 +7,28 @@ const nome = localStorage.getItem("nome");
 const cidade = localStorage.getItem("cidade");
 
 console.log(`${nome} é de ${cidade}`);
+
+
+// Salvar um objeto
+const produto = {
+    nome: "Notebook",
+    preco: 3500,
+    marca: "Positivo"
+}
+
+localStorage.setItem("produto", JSON.stringify(produto));
+
+// Recuperando um objeto
+
+const produtoRecuperadoS = localStorage.getItem("produto");
+console.log(produtoRecuperadoS);
+
+const produtoRecuperadoO = JSON.parse(produtoRecuperadoS);
+
+console.log(produtoRecuperadoO);
+
+
+// Armazenamento com Session
+
+sessionStorage.setItem("jogo", "Mortal Kombat 12");
+console.log(sessionStorage.getItem("jogo"));
