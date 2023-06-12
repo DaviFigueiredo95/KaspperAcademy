b7fb83e243a26bb403158c66e3f672f049669356
Davi <davifigueiredo0195@gmail.com>
2023-06-12 19:02:00 -0300

    ArrayList

---
+++ Backend/02-06-2023/semanaoo/src/listas/principal/TesteLista.java
@@ -0,0 +1,13 @@
+package listas.principal;
+
+import java.util.ArrayList;
+
+public class TesteLista {
+    public static void main(String[] args) {
+        String disciplina1 = "JavaScript";
+        String disciplina2 = "Banco de Dados";
+        String disciplina3 = "Java";
+
+        ArrayList<String> disciplinas = new ArrayList<>();
+    }
+}
