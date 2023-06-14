d691dcf3f7489ac8071cb54058a71bc32103434e
Davi <davifigueiredo0195@gmail.com>
2023-06-14 12:36:37 -0300

    metodo construtor

--- Backend/02-06-2023/semanaoo/src/listas/entidades/Disciplina.java
+++ Backend/02-06-2023/semanaoo/src/listas/entidades/Disciplina.java
@@ -1,5 +1,24 @@
 package listas.entidades;
 
 public class Disciplina {
-    
+    private String nome;
+    private int cargaHoraria;
+
+    public Disciplina(String nome, int cargaHoraria) {
+        this.nome = nome;
+        this.cargaHoraria = cargaHoraria;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
 }
