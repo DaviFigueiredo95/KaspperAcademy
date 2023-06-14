c7305e92daea1fdfd4512f60e80475275cbca1c4
Davi <davifigueiredo0195@gmail.com>
2023-06-14 12:10:44 -0300

    novas adições

--- Backend/02-06-2023/semanaoo/src/listas/principal/TesteLista.java
+++ Backend/02-06-2023/semanaoo/src/listas/principal/TesteLista.java
@@ -7,13 +7,19 @@ public class TesteLista {
         String disciplina1 = "JavaScript";
         String disciplina2 = "Banco de Dados";
         String disciplina3 = "Java";
+        String disciplina4 = "Angular";
 
         ArrayList<String> disciplinas = new ArrayList<>();
         // Adicionar elementos no ArrayList
         disciplinas.add(disciplina1);
         disciplinas.add(disciplina2);
         disciplinas.add(disciplina3);
+        System.out.println(disciplinas);
+        disciplinas.add(0, disciplina4);
+        System.out.println(disciplinas);
 
+        // Remover elemento
+        disciplinas.remove(2);
         System.out.println(disciplinas);
     }
 }
