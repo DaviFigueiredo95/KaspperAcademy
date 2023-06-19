7e8876290a58bb1f6e66d22fbadde26cdf6a6fc9
Davi <davifigueiredo0195@gmail.com>
2023-06-19 20:51:33 -0300

    conexão com banco de dados

---
+++ Backend/SpringBoot/banco/src/main/java/com/kaspperacademy/banco/BancoApplication.java
@@ -0,0 +1,13 @@
+package com.kaspperacademy.banco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoApplication.class, args);
+	}
+
+}
