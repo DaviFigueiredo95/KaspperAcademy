d9720c857fefe108cdae6b3d3de1bbc9d1442172
Davi <davifigueiredo0195@gmail.com>
2023-06-21 22:06:30 -0300

    novo projeto

---
+++ Backend/SpringBoot/praticando/src/main/java/com/kaspper/exercicio/praticando/PraticandoApplication.java
@@ -0,0 +1,13 @@
+package com.kaspper.exercicio.praticando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticandoApplication.class, args);
+	}
+
+}
---
+++ Backend/SpringBoot/praticando/src/test/java/com/kaspper/exercicio/praticando/PraticandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaspper.exercicio.praticando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
