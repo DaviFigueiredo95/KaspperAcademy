270295795813374be3737a15e520fb8d7fbe5229
Davi <davifigueiredo0195@gmail.com>
2023-06-28 15:41:55 -0300

    gerando colunas

--- Backend/SpringBoot/praticando/src/main/java/com/kaspper/exercicio/praticando/model/Endereco.java
+++ Backend/SpringBoot/praticando/src/main/java/com/kaspper/exercicio/praticando/model/Endereco.java
@@ -1,17 +1,41 @@
 package com.kaspper.exercicio.praticando.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "enderecos")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Endereco {
 
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Column(name = "rua", nullable = false, length = 75)
 	private String rua;
 	
+	@Column(name = "numero", nullable = false, length = 5)
 	private String numero;
 	
+	@Column(name = "bairro", nullable = false, length = 75)
 	private String bairro;
 	
+	@Column(name = "cidade", nullable = false, length = 75)
 	private String cidade;
 	
+	@OneToOne
+	@JoinColumn(name = "cliente_id", nullable = false, updatable = false)
 	private Cliente cliente;
 	
 }
