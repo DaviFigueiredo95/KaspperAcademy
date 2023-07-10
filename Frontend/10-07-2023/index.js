372562edaf6ce4eb08a52c02de1d8514d040dc31
Davi <davifigueiredo0195@gmail.com>
2023-07-10 20:37:30 -0300

    typescript

---
+++ Frontend/10-07-2023/index.js
@@ -0,0 +1,7 @@
+// o uso de var, let e const também se aplica em Typescript
+var nome = "Davi de Paula Figueiredo";
+var idade = 28;
+var tocaBaixo = false;
+//let numerosDoCebolinha: number[]
+var numerosDoCebolinha = [20, 24, 26, 28];
+console.log("".concat(nome, " - ").concat(idade, " - ").concat(tocaBaixo, " - ").concat(numerosDoCebolinha.length));
---
+++ Frontend/10-07-2023/index.ts
@@ -0,0 +1,8 @@
+// o uso de var, let e const também se aplica em Typescript
+let nome: string = "Davi de Paula Figueiredo";
+let idade: number = 28;
+let tocaBaixo: boolean = false;
+//let numerosDoCebolinha: number[]
+let numerosDoCebolinha: Array<number> = [20, 24, 26, 28];
+
+console.log(`${nome} - ${idade} - ${tocaBaixo} - ${numerosDoCebolinha.length}`);
