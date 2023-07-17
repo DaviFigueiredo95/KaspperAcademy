666829ad659582b39251580905e078b08d8d72cd
Davi <davifigueiredo0195@gmail.com>
2023-07-17 20:07:37 -0300

    new project

---
+++ Frontend/angular/projeto-kaspper/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/admin/header/header.component';
+import { FooterComponent } from './components/admin/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HeaderComponent,
+    FooterComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
