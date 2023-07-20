bfdcf5bfaa8d8da991918490bca0ea17305335ee
Davi <davifigueiredo0195@gmail.com>
2023-07-19 21:19:38 -0300

    login

--- Frontend/angular/projeto-kaspper/src/app/components/admin/footer/footer.component.ts
+++ Frontend/angular/projeto-kaspper/src/app/components/admin/footer/footer.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { LogoComponent } from "../../shared/logo/logo.component";
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss'],
-  standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule]
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.scss'],
+    standalone: true,
+    imports: [MatToolbarModule, MatButtonModule, MatIconModule, LogoComponent]
 })
 export class FooterComponent {
 
---
+++ Frontend/angular/projeto-kaspper/src/app/components/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, MatInputModule, MatCardModule, MatButtonModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+
+  }
+  ngOnInit(): void {
+    this.form = this.fb
+  }
+
+
+}
