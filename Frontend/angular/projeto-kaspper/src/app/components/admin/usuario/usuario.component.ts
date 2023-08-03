5bbdf30bc1e73500322824a9f40872e5daf5b0ae
Davi <davifigueiredo0195@gmail.com>
2023-08-02 21:31:24 -0300

    tags

--- Frontend/angular/projeto-kaspper/src/app/components/admin/usuario/usuario.component.ts
+++ Frontend/angular/projeto-kaspper/src/app/components/admin/usuario/usuario.component.ts
@@ -3,11 +3,26 @@ import { CommonModule } from '@angular/common';
 import { UsuariosService } from 'src/app/services/usuarios.service';
 import { Usuario } from 'src/app/models/usuario.model';
 import {MatTableModule} from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-usuario',
   standalone: true,
-  imports: [CommonModule, MatTableModule],
+  imports: [CommonModule, 
+    MatTableModule, 
+    MatButtonModule, 
+    FormsModule, 
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatIconModule
+  ],
   templateUrl: './usuario.component.html',
   styleUrls: ['./usuario.component.scss'],
 })
@@ -16,13 +31,35 @@ export class UsuarioComponent implements OnInit {
   usuarios: Usuario[] = [];
   colunas: string[] = ["nome", "email"];
 
+  usuario:Usuario = {};
+
+  form!: FormGroup;
+  fb = inject(FormBuilder);
+
   ngOnInit(): void {
     this.carregaUsuarios();
+    this.createForm();
+  }
+
+  save() {
+    console.log(this.form.value);
+  }
+
+  createForm() {
+    this.form = this.fb.group({
+      id: [''],
+      nome: ['', [Validators.required, Validators.min(5)]],
+      email: ['', [Validators.required, Validators.email]]
+    })
   }
 
   carregaUsuarios() {
     this.usuarioService
                       .getUsuarios()
                       .subscribe((dados) => (this.usuarios = dados));
+  }
+
+  novoUsuario() {
+
   }
 }
